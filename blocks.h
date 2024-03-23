79da407571617d1e5720891ba62a9a4bf7b85d9c
icymanred <reuben.c_p@outlook.com>
2024-03-23 21:54:18 +0000

    added block constant macros

---
+++ blocks.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef BLOCKSDEF
+#define BLOCKSDF
+#define NOR 0
+#define AND 1
+#define OR 2
+#define XOR 3
+#define BUTTON 4
+#define FLIPFLOP 5
+#define LED 6
+#define SOUND 7
+#define CONDUCTOR 8
+#define CUSTOM 9
+#define NAND 10
+#define XNOR 11
+#define RANDOM 12
+#define TEXT 13
+#define TITLE 14
+#define NODE 15
+#define DELAY 16
+#define ANTENNA 17
+
+
+
+
+
+
+
+
+#endif
